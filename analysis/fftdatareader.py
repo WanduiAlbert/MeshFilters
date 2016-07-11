3699e02abaad8698469d158749b1ede065783759
Albert Wandui <albertkamau16@gmail.com>
2016-07-11 11:58:59 -0700

    fixed a bug due to github duplicating sections of my code

--- analysis/fftdatareader.py
+++ analysis/fftdatareader.py
@@ -8,7 +8,6 @@ c = c.value*1e-6 #Speed of light in mm/ns
 R = 0.0234
 c_k, s_k = np.loadtxt('fft_results.dat', unpack=True)
 
-<<<<<<< HEAD
 b0 = c_k[0]
 c_k = c_k[1:-1]
 s_k = s_k[1:-1]
@@ -19,20 +18,6 @@ index = np.arange(1, len(b_k)+1)
 fig, ax = plt.subplots(figsize=(15,10))
 ax.semilogy(index[:10], b_k[:10]/(2*b0),'rd-', label='Fourier Series')
 ax.semilogy(index[:10], R**index[:10],'bo-', label = 'Actual Series')
-=======
-print c_k[0], s_k[0]
-
-b0 = c_k[0]
-c_k = c_k[1:-1]
-s_k = s_k[1:-1]
-b_k = (c_k**2 + s_k**2)
-
-index = np.arange(1, len(b_k)+1)
-print len(index) == len(b_k)
-fig, ax = plt.subplots(figsize=(15,10))
-ax.plot(index[:10], b_k[:10]/(2*b0),'rd-', label='Fourier Series')
-ax.plot(index[:10], R**index[:10],'bo-', label = 'Actual Series')
->>>>>>> 5279165ddd213bbebb1a44f483fca852f01b318a
 ax.set_xlabel('k')
 ax.legend(loc='best')
 ax.set_ylabel(r'$\textrm{b}_{\textrm{k}}$')
